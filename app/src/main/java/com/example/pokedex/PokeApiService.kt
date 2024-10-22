1185c9f67518bbb914aa7afe807173b1a3d390e8
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-10-22 08:22:20 -0300

    TarefasPendentesNaApiService

--- app/src/main/java/com/example/pokedex/PokeApiService.kt
+++ app/src/main/java/com/example/pokedex/PokeApiService.kt
@@ -6,17 +6,23 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PokeApiService {
-
-
+//https://pokeapi.co/api/v2/pokemon/?limit=18&offset=0
     @GET("pokemon/?limit=18&offset=0")
-    fun getPokemonList(): Call<PokeResponse>
+    fun getPokemonList(): Call<PokeResponse> //LISTA
 
     @GET("pokemon/{name}/")
-    suspend fun getPokemonId(@Path("name") name: String): Response<PokemonIdResponse>
+    suspend fun getPokemonId(@Path("name") name: String): Response<PokemonIdResponse> //ID
 
     @GET("pokemon/{id}/")
-    suspend fun getPokemonDetail(@Path("id") id: Int): Response<PokeDto>
+    suspend fun getPokemonDetail(@Path("id") id: Int): Response<PokeDto> //Detalhes (vários)
 
     @GET("pokemon-species/{name}/")
     suspend fun getPokemonColorAndSpecie(@Path("name") name: String): Response<PokemonColorAndSpecieResponse>
+    //Detalhe: cor e espécie (outro serviço)
+
+    //V-Tarefa: Buscar na documentação do kotlin, se essa p$#@% de linguagem suporta uma variável equivalente ao 'variant do VBA.
+    //V-Suporta: variável "Any" resolveria o problema, apesar de ser mais limitada que a 'variant
+    //C-takewp viu
+    //Tarefa: quando puxar a lista, não pegar somente o nome do pokemon, puxar também seu devido ID
+    //Parei nos 6:22 do vídeo "Organizando pacotes", revê-lo do começo.
 }
